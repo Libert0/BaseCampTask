de496e4518a1d2c5358744f35d1f386998a80982
Libert0 <andre.duda7766@gmail.com>
2023-08-02 21:24:34 +0300

    Used static library in the project FileSearch

---
+++ FileSearch/src/FileSearch.cpp
@@ -0,0 +1,22 @@
+﻿#include "FileSearch.h"
+#include "FileSeeker.h"
+#include <string>
+#include <vector>
+
+std::string inputFileName() {
+    std::string filename;
+    std::cout << "Enter filename: ";
+    std::getline(std::cin, filename);
+    return filename;
+}
+
+int main()
+{
+    std:: string filename = inputFileName();
+    FileSeeker fileSearch("C:\\", filename);
+    fileSearch.start();
+    std::this_thread::sleep_for(std::chrono::seconds(5)); 
+    fileSearch.stop();
+
+    return 0;
+}
